0a5a9f4d20ba4a337d50140005fc8d6887ae10fe
lab00 <89362256+laboo25@users.noreply.github.com>
2024-04-28 21:53:00 +0600

    Update dbConnection.js

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 function dbConnection() {
-    mongoose.connect(process.env.MONGO_URI)
+    mongoose.connect('mongodb+srv://laboo:laboo@cluster0.yx4uikx.mongodb.net/videolist?retryWrites=true&w=majority&appName=Cluster0')
         .then(() => console.log('MongoDB connected'))
         .catch(err => console.error('Error connecting to DB:', err));
 }
